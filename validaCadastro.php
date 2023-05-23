606fb14c2b15b04313de3b1b20e91d4763d507a4
Alisson <alisson.paulino@aluno.unifapce.edu.br>
2023-05-23 18:49:37 -0300

    POO e PDO de cadastro

--- validaCadastro.php
+++ validaCadastro.php
@@ -2,70 +2,163 @@
 
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "jobs";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Verifica se os dados foram preenchidos
+    if (isset($_POST['nome']) && isset($_POST['sobrenome']) && isset($_POST['email']) && isset($_POST['senha'])
+    && isset($_POST['cpf']) && isset($_POST['rg']) && isset($_POST['dtNascimento']) && isset($_POST['endereco'])
+    && isset($_POST['cidade']) && isset($_POST['celular'])) {
+        $nome = $_POST['nome'];
+        $sobrenome = $_POST['sobrenome'];
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
+        $cpf = $_POST['cpf'];
+        $rg = $_POST['rg'];
+        $dtNascimento = $_POST['dtNascimento'];
+        $endereco = $_POST['endereco'];
+        $cidade = $_POST['cidade'];
+        $celular = $_POST['celular'];
 
 
-$conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error) {
-    die("Falha na conexão: " . $conn->connect_error);
+class Conexao {
+    private $host = 'localhost';
+    private $dbname = 'jobs';
+    private $usuario = 'root';
+    private $senha = '';
+    
+    public function conectar() {
+        try {
+            $conexao = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->usuario, $this->senha);
+            $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conexao;
+        } catch (PDOException $e) {
+            echo 'Erro na conexão com o banco de dados: ' . $e->getMessage();
+        }
+    }
 }
 
-
-$email = $_POST['email'];
-$sql = "SELECT id FROM cadastro WHERE email = ?";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("s", $email);
-$stmt->execute();
-$result = $stmt->get_result();
-
-if ($result->num_rows > 0) {
-    echo "<div class='cadastrado'>E-mail já cadastrado!<button class='btn-cadastrado' onclick='window.history.back()'>Voltar</button></div>";
-    echo "";
-} else {
+class Usuario {
+    private $conexao;
     
-    $sql = "INSERT INTO cadastro (nome, sobrenome, email, senha, cpf, rg, dtNascimento, endereco, cidade, celular)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-
-   
-    $stmt = $conn->prepare($sql);
-
-    if ($stmt === false) {
-        die("Falha na preparação da declaração: " . $conn->error);
+    public function __construct($conexao) {
+        $this->conexao = $conexao;
     }
-
-    $stmt->bind_param("ssssssssss", $nome, $sobrenome, $email, $senha, $cpf, $rg, $dt_nascimento, $endereco, $cidade, $celular);
-
     
-    $nome = $_POST['nome'];
-    $sobrenome = $_POST['sobrenome'];
-    $senha = $_POST['senha'];
-    $cpf = $_POST['cpf'];
-    $rg = $_POST['rg'];
-    $dt_nascimento = $_POST['dtNascimento'];
-    $endereco = $_POST['endereco'];
-    $cidade = $_POST['cidade'];
-    $celular = $_POST['celular'];
-
+    public function cadastrar($nome, $sobrenome, $email, $senha, $cpf, $rg, $dtNascimento, $endereco, $cidade, $celular) {
+        $sql = "INSERT INTO cadastro (nome, sobrenome, email, senha, cpf, rg, dtNascimento, endereco, cidade, celular) 
+        VALUES (:nome, :sobrenome, :email, :senha, :cpf, :rg, :dtNascimento, :endereco, :cidade, :celular)";
+        
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindParam(':nome', $nome);
+        $stmt->bindParam(':sobrenome', $sobrenome);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':senha', $senha);
+        $stmt->bindParam(':cpf', $cpf);
+        $stmt->bindParam(':rg', $rg);
+        $stmt->bindParam(':dtNascimento', $dtNascimento);
+        $stmt->bindParam(':endereco', $endereco);
+        $stmt->bindParam(':cidade', $cidade);
+        $stmt->bindParam(':celular', $celular);
+        
+        try {
+            $stmt->execute();
+            echo "<script>setTimeout(function(){window.location.href='login.php';}, 3000);</script>";
+            echo "<div class='success'>Cadastro realizado com sucesso!</div>";
+            echo "<div class='loader'></div>";
+            echo "<div class='text'>Você será redirecionado para tela de login</div>";
+        } catch (PDOException $e) {
+            echo 'Erro ao cadastrar usuário: ' . $e->getMessage();
+        }
+    }
  
-    if ($stmt->execute()) {
+    public function listar() {
+        $sql = "SELECT * FROM cadastro";
+        
+        $stmt = $this->conexao->prepare($sql);
+        
+        try {
+            $stmt->execute();
+            $usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $cadastro;
+        } catch (PDOException $e) {
+            echo 'Erro ao listar usuários: ' . $e->getMessage();
+        }
+    }
+    
+    public function atualizar($id, $nome, $senha, $cpf_cnpj, $contato) {
+        $sql = "UPDATE cadastrp SET nome = :nome, senha = :senha, cpf_cnpj = :cpf_cnpj WHERE id = :id";
+        
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindParam(':nome', $nome);
+        $stmt->bindParam(':senha', $email);
+        $stmt->bindParam(':cpf_cnpj', $cpf_cnpj);
+        $stmt->bindParam(':contato', $contato);
+        $stmt->bindParam(':id', $id);
+        
+        try {
+            $stmt->execute();
+            echo 'Usuário atualizado com sucesso!';
+        } catch (PDOException $e) {
+            echo 'Erro ao atualizar usuário: ' . $e->getMessage();
+        }
+    }
+    
+    public function excluir($id) {
+        $sql = "DELETE FROM cadastro WHERE id = :id";
+        
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindParam(':id', $id);
+        
+        try {
+            $stmt->execute();
+            echo 'Usuário excluído com sucesso!';
+        } catch (PDOException $e) {
+            echo 'Erro ao excluir usuário: ' . $e->getMessage();
+        }
+    }
+}
 
-        echo "<script>setTimeout(function(){window.location.href='login.php';}, 3000);</script>";
-        echo "<div class='success'>Cadastro realizado com sucesso!</div>";
-        echo "<div class='loader'></div>";
-        echo "<div class='text'>Você será redirecionado para tela de login</div>";
-    } else {
-        echo "Erro ao cadastrar: " . $stmt->error;
+class Validacao {
+
+    private $conexao;
+
+    public function __construct($conexao) {
+        $this->conexao = $conexao;
     }
 
-    $stmt->close();
+    public function checkEmail($email, $nome, $sobrenome, $senha, $cpf, $rg, $dtNascimento, $endereco, $cidade, $celular){
+        $sql = "SELECT id FROM cadastro WHERE email = :email";
+    
+        $stmt = $this->conexao->prepare($sql);
+        
+        $stmt->bindValue(':email', $email);
+
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($result) {
+            echo "<div class='cadastrado'>E-mail já cadastrado!<button class='btn-cadastrado' onclick='window.history.back()'>Voltar</button></div>";
+            echo "";
+        } else {
+            $cadastro = new Usuario($this->conexao);
+            $cadastro->cadastrar($nome, $sobrenome, $email, $senha, $cpf, $rg, $dtNascimento, $endereco, $cidade, $celular);
+        }
+    }
+}
+
+$conexao = new Conexao();
+
+$valida = new Validacao($conexao->conectar());
+
+$valida->checkEmail($email, $nome, $sobrenome, $senha, $cpf, $rg, $dtNascimento, $endereco, $cidade, $celular);
+
+    } else {
+        echo 'Por favor, preencha todos os campos do formulário.';
+    } 
+} else {
+    echo 'O formulário não foi enviado.';
 }
 
 
-$conn->close();
 ?>
 
 
@@ -131,4 +224,3 @@ align-items:center;
 }
 
 </style>
-
